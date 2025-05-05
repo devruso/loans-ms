7a2ab44086a6f04d380357fae721118488fdf29b
jamilson junior <jamilson.junior@byd.com>
2025-05-05 11:52:04 -0300

    fix: correct loan dto

--- src/main/java/com/ms/loans/dto/LoanDto.java
+++ src/main/java/com/ms/loans/dto/LoanDto.java
@@ -1,7 +1,9 @@
 package com.ms.loans.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
@@ -9,19 +11,20 @@ import lombok.Data;
 @Data
 public class LoanDto {
 
-    @NotNull
+    @NotNull @NotBlank
     private String mobileNumber;
 
     @NotNull
     private String loanNumber;
 
-    @NotNull
     private String loanType;
 
-    @NotNull
+    @Positive
     private int totalLoan;
 
     @PositiveOrZero
-    private int outstandingAmount = 0;
+    private int amountPaid;
+
+    private int outstandingAmount;
 
 }
